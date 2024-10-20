e857edadbaba756a4bc300e0c451b70c098b032f
Boubouteat <borhanetixto@gmail.com>
2024-10-20 22:05:59 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,12 @@ import { useEffect, useState } from 'react'
 
 // Define the interface for user data
 interface UserData {
-  
+  id: number;
   first_name: string;
   last_name?: string;
+  username?: string;
+  language_code: string;
+  is_premium?: boolean;
 }
 
 export default function Home() {
@@ -25,10 +28,12 @@ export default function Home() {
         <>
           <h1 className="text-2xl font-bold mb-4">User Data</h1>
           <ul>
-            
+            <li>ID: {userData.id}</li>
             <li>First Name: {userData.first_name}</li>
             <li>Last Name: {userData.last_name || 'N/A'}</li>
             <li>Username: {userData.username || 'N/A'}</li>
+            <li>Language Code: {userData.language_code}</li>
+            <li>Is Premium: {userData.is_premium ? 'Yes' : 'No'}</li>
           </ul>
         </>
       ) : (
