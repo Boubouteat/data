a43ca15ed3a6a5a5abe642da8ced08da44f9f9b5
Boubouteat <borhanetixto@gmail.com>
2024-10-21 23:55:36 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -75,7 +75,7 @@ export default function Home() {
       {isAdmin && (
         <div className="fixed top-4 right-4 cursor-pointer" onClick={toggleModal}>
           <img
-            src="http://www.w3.org/2000/svg" // أيقونة المفك من Bootstrap
+            src="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/icons/shield.svg" // أيقونة المفك من Bootstrap
             alt="Settings"
             className="w-8 h-8"
             style={{ filter: 'invert(100%)', color: '#00BFFF' }} // لون المفك: أزرق فاتح وواضح
